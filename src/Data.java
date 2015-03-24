1a9bcd653e5ddb54b05002717035a3947aef0a04
WouterM <woutermenninga@gmail.com>
2015-03-24 22:22:17 +0100

    some small stuff for analytic server, and addes some attributes to Data

--- src/Data.java
+++ src/Data.java
@@ -2,6 +2,7 @@ package com.nativedevelopment.smartgrid;
 
 
 public class Data {
+	public String deviceId;
 	/** Time of data */
 	public long unixTimestamp;
 	/** Usage in unit/second */
@@ -12,6 +13,8 @@ public class Data {
 	public double predictedUsage;
 	/** Predicted production in next time interval */
 	public double predictedProduction;
+	/** Potential production in next time interval */
+	public double potentialProduction;
 	/** Length in seconds of next time interval */
 	public int predictedTimeInterval;
 }
