37b25bbf6c1763435be89129fe37ed67d9890180
Randy Wind <Randy Wind>
2017-02-14 14:39:10 +0100

    Add context to adress.

--- src/com/nativedevelopment/smartgrid/Address.java
+++ src/com/nativedevelopment/smartgrid/Address.java
@@ -4,11 +4,13 @@ import java.util.UUID;
 
 public class Address implements IAddress {
 	private UUID a_oIdentifier = null;
+	private UUID a_oContext = null;
 	private String a_sHost = null;
 	private int a_iPort = -1;
 
-	public Address(UUID oIdentifier, String sHost, int iPort) {
+	public Address(UUID oIdentifier, UUID a_oContext, String sHost, int iPort) {
 		a_oIdentifier = oIdentifier;
+		a_oContext = a_oContext;
 		a_sHost = sHost;
 		a_iPort = iPort;
 	}
@@ -16,6 +18,11 @@ public class Address implements IAddress {
 	@Override
 	public UUID GetIdentifier() {
 		return a_oIdentifier;
+	}
+
+	@Override
+	public UUID GetContext() {
+		return a_oContext;
 	}
 
 	@Override
--- src/com/nativedevelopment/smartgrid/IAddress.java
+++ src/com/nativedevelopment/smartgrid/IAddress.java
@@ -5,6 +5,7 @@ import java.util.UUID;
 
 public interface IAddress extends Serializable {
 	public UUID GetIdentifier();
+	public UUID GetContext();
 	public String GetHost();
 	public int GetPort();
 }
